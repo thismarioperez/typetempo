e223636c61ccaacbf16f9d95af076c2e112f0142
Mario Perez <mario.perez415@gmail.com>
2024-11-16 20:43:09 -0500

    Fix server build

--- apps/server/src/utils/logger.ts
+++ apps/server/src/utils/logger.ts
@@ -1,5 +1,5 @@
 import winston from "winston";
-import { config } from "@/config";
+import { config } from "../config";
 
 export const logger = winston.createLogger({
     level: config.nodeEnv === "production" ? "info" : "debug",
