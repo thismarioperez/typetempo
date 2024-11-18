d1f1f86685ac135ff683eef877d1bea0da3f41bc
Mario Perez <mario.perez415@gmail.com>
2024-11-18 12:08:54 -0500

    Update package naming convention to use repo name

--- packages/database/.eslintrc.cjs
+++ packages/database/.eslintrc.cjs
@@ -2,7 +2,7 @@
 module.exports = {
     root: true,
     ignorePatterns: [".eslintrc.cjs"],
-    extends: ["@repo/eslint-config/index.js"],
+    extends: ["@typetempo/eslint-config/index.js"],
     parser: "@typescript-eslint/parser",
     parserOptions: {
         project: true,
