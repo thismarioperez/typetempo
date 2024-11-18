be422576bcd111c2b151c084f166f788daceab2d
Mario Perez <mario.perez415@gmail.com>
2024-11-17 19:41:18 -0500

    Setup db, prisma, and docker compose for local dev

---
+++ packages/database/.eslintrc.cjs
@@ -0,0 +1,13 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+    root: true,
+    ignorePatterns: [".eslintrc.cjs"],
+    extends: ["@repo/eslint-config/index.js"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: true,
+    },
+    rules: {
+        "no-var": "off",
+    },
+};
