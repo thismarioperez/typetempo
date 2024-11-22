b986d8475f130bf24d481737f4d5b683a58e7e2e
Mario Perez <mario.perez415@gmail.com>
2024-11-22 16:14:26 -0500

    Show the typing test on home view

--- apps/web/src/views/HomeView.vue
+++ apps/web/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
+import TheTypingTest from "@/components/TheTypingTest.vue";
 </script>
 
 <template>
     <main>
-        <TheWelcome />
+        <TheTypingTest />
     </main>
 </template>
