ea3061e42eb899942bf6ca27674458c5a910e1bb
Mario Perez <mario.perez415@gmail.com>
2024-11-22 13:19:20 -0500

    Replace about page with settings page

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -12,7 +12,7 @@ import HelloWorld from "./components/HelloWorld.vue";
 
             <nav>
                 <RouterLink to="/">Home</RouterLink>
-                <RouterLink to="/about">About</RouterLink>
+                <RouterLink to="/settings">Settings</RouterLink>
             </nav>
         </div>
     </header>
