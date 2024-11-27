550b750e1c5a8651c48803c3602053a323e2471c
Mario Perez <mario.perez415@gmail.com>
2024-11-27 11:33:07 -0500

    Fix settings reactivity

--- apps/web/src/stores/settings.ts
+++ apps/web/src/stores/settings.ts
@@ -1,15 +1,15 @@
-import { reactive } from "vue";
+import { reactive, ref } from "vue";
 import { defineStore } from "pinia";
 import type { Settings } from "@/models";
 import { DEFAULT_SETTINGS } from "@/models";
 
 export const useSettingsStore = defineStore("settings", () => {
-    const settings = reactive<Settings>(DEFAULT_SETTINGS);
+    const settings = ref(reactive<Settings>(DEFAULT_SETTINGS));
 
     const updateSettings = (newSettings: Partial<Settings>) => {
         Object.entries(newSettings).forEach(([key, value]) => {
             // @ts-ignore
-            settings[key] = value;
+            settings.value[key] = value;
         });
     };
 
