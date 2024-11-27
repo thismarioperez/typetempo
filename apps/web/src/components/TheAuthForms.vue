8f15e63522821a9bbdbe561ce39541eeb575ac00
Mario Perez <mario.perez415@gmail.com>
2024-11-27 00:23:22 -0500

    Setup auth forms

---
+++ apps/web/src/components/TheAuthForms.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { reactive, ref } from "vue";
+import { useAuthStore } from "@/stores/auth";
+import { storeToRefs } from "pinia";
+import type { LoginCredentials, RegisterData } from "@typetempo/models";
+const { authResponse, isAuthenticated } = storeToRefs(useAuthStore());
+const { login, register } = useAuthStore();
+const mode = ref<"login" | "register">("login");
+const loginData = reactive<LoginCredentials>({ email: "", password: "" });
+const registerData = reactive<RegisterData>({ email: "", name: "", password: "" });
+
+const handleLoginSubmit = async () => {
+    const response = await login(loginData);
+
+    if (response) {
+        // login successful
+        console.log("login successful");
+    }
+};
+
+const handleRegisterSubmit = async () => {
+    const response = await register(registerData);
+
+    if (response) {
+        // register successful
+        console.log("register successful");
+    }
+};
+</script>
+
+<template>
+    <div v-if="!isAuthenticated">
+        <form v-if="mode === 'login'" ref="loginForm" @submit.prevent="handleLoginSubmit">
+            <input id="email" type="text" name="email" placeholder="email" v-model="loginData.email" />
+            <input id="password" type="password" name="password" placeholder="password" v-model="loginData.password" />
+            <button type="submit">Login</button>
+        </form>
+        <form v-if="mode === 'register'" ref="registerForm" @submit.prevent="handleRegisterSubmit">
+            <input id="name" type="text" name="name" placeholder="name" v-model="registerData.name" />
+            <input id="email" type="email" name="email" placeholder="email" v-model="registerData.email" />
+            <input
+                id="password"
+                type="password"
+                name="password"
+                placeholder="password"
+                v-model="registerData.password"
+            />
+            <button type="submit">Register</button>
+        </form>
+        <button v-if="mode === 'login'" @click="mode = 'register'">Register instead</button>
+        <button v-if="mode === 'register'" @click="mode = 'login'">Login instead</button>
+    </div>
+    <div v-else>
+        <p>You are already logged in</p>
+    </div>
+</template>
+
+<style scoped></style>
