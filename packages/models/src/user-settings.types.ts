5832acaa4f1aabb5446bd667b28a9487e187b579
Mario Perez <mario.perez415@gmail.com>
2024-11-26 21:33:00 -0500

    Create user settings response type

--- packages/models/src/user-settings.types.ts
+++ packages/models/src/user-settings.types.ts
@@ -1,3 +1,8 @@
 import { type UserSettings as BaseUserSettings } from "@typetempo/db";
 
 export type UserSettings = Partial<BaseUserSettings>;
+
+export type UserSettingsResponse = {
+    message: string;
+    userSettings: Pick<UserSettings, "theme" | "language" | "wordLimit">;
+};
